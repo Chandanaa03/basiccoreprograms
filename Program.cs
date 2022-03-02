60239b1232d53fefed1b8763eb7648902cb10ee2
chandana03 <chandanareshma01@gmail.com>
2022-03-02 23:23:31 +0530

    flipcoin

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace basicprograms
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //username.displayusername();
+            flip_coin.flipcoinpercentage();
+            //power2.displaypower();
+            
+        }
+    }
+}
