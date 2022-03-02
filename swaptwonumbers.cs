0db12fdf4169fc5ddb19f4b31322bb3a645d5412
chandana03 <chandanareshma01@gmail.com>
2022-03-03 00:38:48 +0530

    swap two numbers

---
+++ swaptwonumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicprograms
+{
+    public class swaptwonumbers
+    {
+        public static void swapnumber()
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a = " + a + "  b = " + b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("After swap a = " + a + "  b = " + b);
+
+
+        }
+    }
+}
