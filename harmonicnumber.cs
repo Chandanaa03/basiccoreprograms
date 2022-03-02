3cdccd87a93a348828c33b289e166b1ac075cddf
chandana03 <chandanareshma01@gmail.com>
2022-03-03 00:22:33 +0530

    calculate harmonic

---
+++ harmonicnumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicprograms
+{
+    public class harmonicnumber
+    {
+        public static void harmonic( )
+        {
+            int i;
+            int n = Convert.ToInt32(Console.ReadLine());
+            float sum = 0;
+            for (i = 1; i <= n; i++)
+            {
+                Console.WriteLine("1/" + i);
+
+
+                sum = sum + (float)1 / i;
+
+
+
+                
+
+            }
+
+            Console.WriteLine("harmonic mean upto " + n + " = " + sum);
+        }
+    }
+}
