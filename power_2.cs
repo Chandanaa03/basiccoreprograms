a0598ca48d559e96d25f5e2e2a41455e1a3d028e
chandana03 <chandanareshma01@gmail.com>
2022-03-02 23:46:19 +0530

    check power_2

---
+++ power_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicprograms
+{
+    public class power_2
+    {
+        public static void calculatepower()
+        {
+            int pow = 1;
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n <= 31)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    pow = pow * 2;
+                }
+                Console.WriteLine(pow);
+            }
+        }
+
+    }
+}
