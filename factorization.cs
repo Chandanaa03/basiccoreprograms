f610869c5533eb2d4fceb94e6a70c32f84c317ce
chandana03 <chandanareshma01@gmail.com>
2022-03-03 19:35:04 +0530

    prime factors

---
+++ factorization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicprograms
+{
+    public class factorization
+    {
+        public static void primefactor()
+        {
+            int a, b;
+            Console.WriteLine("please enter the integer");
+            a = Convert.ToInt32(Console.ReadLine());
+            for (b = 1; b <= a; b++)
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine(b + " is a factor of " + a);
+                }
+            }
+        }
+    }
+}
