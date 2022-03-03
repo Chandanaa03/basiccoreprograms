c360b9411fa33eb3e10221e8bb1d37337fc5386e
chandana03 <chandanareshma01@gmail.com>
2022-03-03 15:48:00 +0530

    check even and odd numbers

---
+++ evenandodd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicprograms
+{
+    public class evenandodd
+    {
+        public static void checkevenandodd()
+        {
+            int i;
+
+            Console.WriteLine("Enter the number : ");
+            i = Convert.ToInt32(Console.ReadLine());
+            if  (i % 2 == 0)
+            {
+                Console.WriteLine("The given number is even");
+            }
+
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+        }
+    }
+}
