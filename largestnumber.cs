450d53d11ba8658d5c8d3e2405504a1d4af5616f
chandana03 <chandanareshma01@gmail.com>
2022-03-03 16:26:06 +0530

    largest number among three numbers

---
+++ largestnumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicprograms
+{
+    public class largestnumber
+    {
+        public static void largest_number()
+        {
+           int num1, num2, num3;
+            Console.WriteLine("Enter the numbers: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                Console.WriteLine("num1 is largest number");
+
+            }
+            else if(num2 >= num1 && num2 >= num3)
+            {
+                Console.WriteLine("num2 is largest");
+            }
+            else
+            {
+                Console.WriteLine("num3 is largest");
+            }
+        }
+    }
+}
